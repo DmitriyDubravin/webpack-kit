37e13090f1ac79026b8d314bf35085880374307b
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2017-08-30 21:25:59 +0300

    js binder

--- dev/js/global.js
+++ dev/js/global.js
@@ -1,7 +1,9 @@
 require('../css/style.scss');
-require('../html/home.html');
-require('../html/about.html');
-require('../html/contact.html');
+// require('../html/home.html');
+// require('../html/about.html');
+// require('../html/contact.html');
+
+import func from './functions';
 
 var {hi, event} = require('./messages');
 import Button from './button';
@@ -16,38 +18,117 @@ if(dev) {
 }
 
 
-var appHolder = document.createElement('div');
-appHolder.id = 'app';
-document.body.appendChild(appHolder);
+let tests = true;
 
-var newMessage = () => (`<p>${hi}, ${event}</p>`);
-var newButton = () => (Button.button);
-var newMath = () => (multiply(3,2));
+let elementsToFind = [];
+let wasFound = {};
 
-
-var app = document.querySelector('#app');
-app.innerHTML = dev ? newMath() : newButton();
-
-if(dev) {
-	console.log('dev');
-} else {
-	console.log('prod');
+let bound = {
+	// 'elementToFind': ['correspondingFunction','anotherCorrespondingFunction']
+	'.header': ['test'],
+};
+for(var key in bound) {
+	if(bound.hasOwnProperty(key)) elementsToFind.push(key);
 }
 
-// Button.attachEl();
-
-console.log('test3');
-
-
-function getComponent() {
-	return import('lodash').then(_ => {
-		var element = document.createElement('div');
-		element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-		return element;
-	}).catch(error => 'An error occurred while loading the component');
+function plugInScripts(module) {
+	elementsToFind.forEach(function(selector) {
+		if(wasFound[selector]) {
+			if(tests) console.log(`+ ${selector} --> ${bound[selector]}`);
+			bound[selector].forEach(script => {
+				module[script]();
+			});
+		} else {
+			if(tests) console.log(`- ${selector}`);
+		}
+	});
 }
 
-getComponent().then(component => {
-	document.body.appendChild(component);
-});
+function binder(elementsToFind, modulesToPlugIn) {
+
+	// let wasFound = {};
+	elementsToFind.forEach((selector, index, arr) => {
+		const selectorType = selector.slice(0,1).toLowerCase();
+		const isFound = [...document.querySelectorAll(elementsToFind.join(','))].some(element => {
+			if(selectorType === '.') { // if class
+				return (` ${element.className} `).indexOf(` ${selector.slice(1)} `) > -1;
+			} else if(selectorType === '#') { // if id
+				return element.id.indexOf(selector.slice(1)) > -1;
+			} else if(selectorType === '[') { // if data-attribute
+				return element.hasAttribute(selector.slice(1,-1));
+			} else {
+				return (element.tagName).indexOf(selector) > -1;
+			}
+			// console.log(element); // too many elementst? learn 'some' method ???
+		});
+		wasFound[selector] = isFound;
+		if(index === arr.length - 1) {
+			plugInScripts(modulesToPlugIn); // callback
+		}
+	});
+	// return wasFound;
+}
+
+function checkFuncionSpeed(f, fArgs) {
+	let t0 = performance.now();
+	let result = f(fArgs);
+	let t1 = performance.now();
+	console.log("Elements were found in " + (t1 - t0) + " milliseconds.");
+	return result;
+}
+
+// let wasFound = binder(elementsToFind);
+// let wasFound = checkFuncionSpeed(binder, elementsToFind);
+binder(elementsToFind, func);
+
+// isTouch
+if('ontouchstart' in window) document.body.className += ' touch';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var appHolder = document.createElement('div');
+// appHolder.id = 'app';
+// document.body.appendChild(appHolder);
+
+// var newMessage = () => (`<p>${hi}, ${event}</p>`);
+// var newButton = () => (Button.button);
+// var newMath = () => (multiply(3,2));
+
+
+// var app = document.querySelector('#app');
+// app.innerHTML = dev ? newMath() : newButton();
+
+// if(dev) {
+// 	console.log('dev');
+// } else {
+// 	console.log('prod');
+// }
+
+// // Button.attachEl();
+
+// console.log('test3');
+
+
+// function getComponent() {
+// 	return import('lodash').then(_ => {
+// 		var element = document.createElement('div');
+// 		element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// 		return element;
+// 	}).catch(error => 'An error occurred while loading the component');
+// }
+
+// getComponent().then(component => {
+// 	document.body.appendChild(component);
+// });
 
