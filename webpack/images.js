dbfc2c26eacc9ac4a1338f29bb049b4c53a9a1ed
DmitriyDubravin <dmitriy.dubravin@gmail.com>
2017-09-25 08:37:05 +0300

    image mins try

--- webpack/images.js
+++ webpack/images.js
@@ -16,6 +16,30 @@ module.exports = dev
 		test: /\.(png|svg|jpg|gif)$/,
 		use: [
 			{
+				loader: 'image-webpack',
+				query: {
+					optimizationLevel: 7,
+					pngquant: {
+						quality: '65-90',
+						speed: 4
+					},
+					mozjpeg: {
+						progressive: true,
+						quality: 65
+					},
+					svgo:{
+						plugins: [
+							{
+								removeViewBox: false
+							},
+							{
+								removeEmptyAttrs: false
+							}
+						]
+					}
+				}
+			},
+			{
 				loader: 'file-loader',
 				options: {
 					name: '[name].[ext]',
